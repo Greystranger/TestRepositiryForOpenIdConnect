c861e5239c930a6f69bc693b9fd767b7c87f305c
Yatsenko, Evgeniy <Yatsenko@extedo.com>
2018-09-08 13:01:55 +0200

    Add viewModel for role management

---
+++ repos/AspNetCoreWithIdentity/AspNetCoreWithIdentity/ViewModels/ChangeRoleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreWithIdentity.ViewModels
+{
+    public class ChangeRoleViewModel
+    {
+        public string UserId { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public IEnumerable<IdentityRole> AllRoles { get; set; } = new List<IdentityRole>();
+
+        public IEnumerable<string> UserRoles { get; set; } = new List<string>();
+    }
+}
