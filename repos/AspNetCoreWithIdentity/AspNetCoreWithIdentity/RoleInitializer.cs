cc6c5ea41b2b4097fe275254b591a5c3b8abb665
Yatsenko, Evgeniy <Yatsenko@extedo.com>
2018-09-08 20:06:52 +0200

    Add RoleInitializer class uses for initialization user when app starts

---
+++ repos/AspNetCoreWithIdentity/AspNetCoreWithIdentity/RoleInitializer.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using AspNetCoreWithIdentity.Models;
+
+namespace AspNetCoreWithIdentity
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "admin@gmail.com";
+            string password = "_Aa123456";
+
+            if (await roleManager.FindByNameAsync("administrator") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("administrator"));
+            }
+
+            if (await roleManager.FindByNameAsync("user") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("user"));
+            }
+
+            if (await userManager.FindByEmailAsync(adminEmail) == null)
+            {
+                var admin = new ApplicationUser
+                {
+                    Email = adminEmail,
+                    UserName = adminEmail
+                };
+
+                var createUserResult = await userManager.CreateAsync(admin, password);
+                if (createUserResult.Succeeded)
+                {
+                    await userManager.AddToRolesAsync(admin, new [] { "administrator", "user" });
+                }
+            }
+        }
+    }
+}
