11fc11f3eb98d791d29be17fa8d3147e6dfa167d
Yatsenko, Evgeniy <Yatsenko@extedo.com>
2018-09-12 11:27:37 +0200

    Add CorsClientApp

--- repos/CorsTestApplication/CorsClientApp/Startup.cs
+++ repos/CorsTestApplication/CorsClientApp/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace CorsTestApplication
+namespace CorsClientApp
 {
     public class Startup
     {
@@ -15,7 +15,6 @@ namespace CorsTestApplication
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,17 +25,8 @@ namespace CorsTestApplication
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseCors(builder =>
-            {
-                builder.AllowAnyHeader();
-                builder.AllowAnyMethod();
-                builder.AllowAnyOrigin();
-            });
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
